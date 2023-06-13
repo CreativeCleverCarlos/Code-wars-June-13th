10ecbb2441cae5baebd0059bea9952b4bbeb23bb
Carlos Campbell <carlos.g.campbell@gmail.com>
2023-06-13 15:40:41 -0400

    Full kata

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/**
+ For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+**/
+
+function well(x){
+    let check = x.filter(good => good == 'bad' ? 'I smell a series' : 'Publish')
+    console.log(check)
+}
+
+well(['bad', 'bad', 'bad'])
+
+/**
+ One type of answer
+
+ function well(x) {
+  switch (x.filter(i => i === 'good').length) {
+    case 0:
+      return 'Fail!'
+    case 1:
+    case 2:
+      return 'Publish!'
+    default:
+      return 'I smell a series!'
+  }
+}
+**/
+
+/**
+ 
+Another type of answer
+ 
+const well = x => {
+  const good_count = x.filter(x => x == 'good').length;
+  return good_count < 1 ? 'Fail!' : 
+         good_count < 3 ? 'Publish!' : 'I smell a series!';
+}
+**/
